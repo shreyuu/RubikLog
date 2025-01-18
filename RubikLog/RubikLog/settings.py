f35ca9616c24f140a40777b7bce521a1fe8b815f
Shreyash Meshram <91451934+shreyuu@users.noreply.github.com>
2025-01-18 23:29:54 +0530

    Add frontend setup with Tailwind CSS, initial React components, and testing configuration

--- RubikLog/RubikLog/settings.py
+++ RubikLog/RubikLog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tracker',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
 
 ROOT_URLCONF = 'RubikLog.urls'
 
